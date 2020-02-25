ab4d2e3eaa304f8c19524c49cf74be9631ae7b27
Spitfire_x86 <pingwin44@gmail.com>
2020-02-25 16:09:28 +0100

    Add unsafe overloads to Memory

--- Ray2Mod/Utils/Memory.cs
+++ Ray2Mod/Utils/Memory.cs
@@ -16,11 +16,31 @@ namespace Ray2Mod.Utils
             return address;
         }
 
+        public static unsafe void* GetPointerAtOffset(int address, params int[] offsets)
+        {
+            foreach (int offset in offsets)
+            {
+                address = *(int*)address;
+                address += offset;
+            }
+
+            return (void*)address;
+        }
+
         public static byte[] GetBytes(IntPtr address, int length)
         {
             byte[] bytes = new byte[length];
             for (int i = 0; i < length; i++)
                 bytes[i] = Marshal.ReadByte(address + i);
+
+            return bytes;
+        }
+
+        public static unsafe byte[] GetBytes(byte* address, int length)
+        {
+            byte[] bytes = new byte[length];
+            for (int i = 0; i < length; i++)
+                bytes[i] = *(address + i);
 
             return bytes;
         }
