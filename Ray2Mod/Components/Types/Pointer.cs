0966d67c05df67af3998fb6fc166aa9ccd8b535f
Spitfire_x86 <pingwin44@gmail.com>
2020-03-02 23:42:32 +0100

    Add Pointer wrapper

---
+++ Ray2Mod/Components/Types/Pointer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ray2Mod.Components.Types
+{
+    public unsafe class Pointer<T> where T : unmanaged
+    {
+        public Pointer(int pointer)
+        {
+            IntPtr = (IntPtr)pointer;
+        }
+
+        public Pointer(IntPtr intPtr)
+        {
+            IntPtr = intPtr;
+        }
+
+        public Pointer(T* structPtr)
+        {
+            IntPtr = (IntPtr)structPtr;
+        }
+
+        private IntPtr IntPtr { get; }
+        public T* StructPtr => (T*)IntPtr;
+
+        public static implicit operator T*(Pointer<T> ptr) => ptr.StructPtr;
+        public static implicit operator IntPtr(Pointer<T> ptr) => ptr.IntPtr;
+        public static implicit operator int(Pointer<T> ptr) => (int)ptr.IntPtr;
+
+        public static implicit operator Pointer<T>(T* structPtr) => new Pointer<T>(structPtr);
+    }
+} 
