38a6ec5d3440ef63d4a0a469d1ceb6f9f06861ec
Spitfire_x86 <pingwin44@gmail.com>
2020-04-10 14:08:41 +0200

    Add TextOverlays sample

--- samples/TextOverlays/Properties/AssemblyInfo.cs
+++ samples/TextOverlays/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ray2Mod")]
+[assembly: AssemblyTitle("TextOverlays")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rayman 2 Modding API")]
+[assembly: AssemblyProduct("TextOverlays")]
 [assembly: AssemblyCopyright("Copyright © RayTools 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a88b3b19-48b3-4d6b-a217-8af81926be25")]
+[assembly: Guid("0309fe7d-fc81-4ad8-bbbf-c10b37fd935c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.32.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ samples/TextOverlays/TextOverlays.cs
@@ -0,0 +1,67 @@
+﻿using Ray2Mod;
+using Ray2Mod.Components.Text;
+
+namespace TextOverlays
+{
+    public class TextOverlays : IMod
+    {
+        private RemoteInterface Interface { get; set; }
+
+        public void Run(RemoteInterface remoteInterface)
+        {
+            Interface = remoteInterface;
+
+            // Regular, static text.
+            // The X and Y values are coordinates on the screen space, mapped to a 1000x1000 area.
+            TextOverlay plain = new TextOverlay("Regular text", 10, 5 ,5).Show();
+
+            // Dynamically updating text.
+            // The delegate TextOverlay.UpdateText is evaluated on every frame (if assigned).
+            // It can be assigned instead of text in the constructor, or later at any point.
+            int frames = 0;
+            TextOverlay counter = new TextOverlay((previousText) =>
+            {
+                string newText = $"Frame counter={frames}";
+                frames++;
+                return newText;
+            }, 10, 5 ,35).Show();
+
+            // Previous text is passed to the UpdateText method as a parameter.
+            // This example appends 1 dot to the text every 10 frames, up to a total of 20.
+            // It also uses the frame counter from the previous example.
+            int dots = 0;
+            TextOverlay append = new TextOverlay("Some dots", 10, 5, 65).Show();
+            append.UpdateText = (text) =>
+            {
+                if (frames % 10 == 0 && dots < 20)
+                {
+                    text += '.';
+                    dots++;
+                }
+                return text;
+            };
+
+            // The delegate TextOverlay.UpdateProperties can be used for advanced text manipulation.
+            // It is evaluated on every frame, after UpdateText.
+            // The TextOverlay object itself is passed as a parameter.
+            // This lets you reuse functions and delegates for multiple text objects.
+            plain.UpdateProperties = MoveTextOffscreen;
+        }
+
+        // This example will move the text object to the right
+        // and hide it once it's no longer visible on screen.
+        private void MoveTextOffscreen(TextOverlay overlay)
+        {
+            overlay.X += 3;
+            Interface.Log($"Text overlay visible at X:{overlay.X}");
+            if (overlay.X > 1000)
+            {
+                // Note: While the text overlay is hidden, both UpdateText and UpdateProperties
+                // are _not_ evaluated - notice the "Text overlay visible" messages
+                // are no longer sent after calling Hide().
+                overlay.Hide();
+                Interface.Log($"Text overlay hidden");
+            }
+        }
+    }
+}
