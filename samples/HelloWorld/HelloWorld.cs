67969d92ba239faf806a6a4097506f16063f6cfe
Spitfire_x86 <pingwin44@gmail.com>
2020-04-10 14:08:10 +0200

    Add log example to HelloWorld

--- samples/HelloWorld/HelloWorld.cs
+++ samples/HelloWorld/HelloWorld.cs
@@ -13,6 +13,9 @@ namespace HelloWorld
             // The X and Y values are coordinates on the screen space, mapped to a 1000x1000 area.
             TextOverlay hello = new TextOverlay("Hello World", 10, 5, 5).Show();
             TextOverlay colors = new TextOverlay("This text is red and transparent".Red(), 10, 5, 30, 180).Show();
+
+            // Use RemoteInterface.Log() to display text in the mod loader console.
+            remoteInterface.Log("Hello World");
         }
     }
 }
