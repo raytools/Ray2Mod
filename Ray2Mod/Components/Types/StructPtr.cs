82d65b8ed8529f85b6a22ba212e09547179f5a6e
Spitfire_x86 <pingwin44@gmail.com>
2020-06-21 00:48:42 +0200

    Add generic StructPtr

--- Ray2Mod/Components/Types/StructPtr.cs
+++ Ray2Mod/Components/Types/StructPtr.cs
@@ -1,8 +1,43 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using Ray2Mod.Utils;
 
 namespace Ray2Mod.Components.Types
 {
+    public unsafe class StructPtr<T> : IDisposable where T : unmanaged
+    {
+        public StructPtr(T obj)
+        {
+            Pointer = Memory.Malloc<T>(Marshal.SizeOf(obj));
+            Marshal.StructureToPtr(obj, (IntPtr)Pointer, false);
+        }
+
+        public T* Pointer { get; private set; }
+
+        public static implicit operator T*(StructPtr<T> ptr) => ptr.Pointer;
+
+        private void ReleaseUnmanagedResources()
+        {
+            if (Pointer != null)
+            {
+                Memory.Free(Pointer);
+                Pointer = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~StructPtr()
+        {
+            ReleaseUnmanagedResources();
+        }
+    }
+
+    [Obsolete("This class is obsolete - use StructPtr<T> instead.")]
     public class StructPtr : IDisposable
     {
         public StructPtr(object obj)
